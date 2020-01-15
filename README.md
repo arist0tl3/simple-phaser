96ddf59ff07a1ef2cc8400090496a329f6d42f45
Sean <sean@vigorwebsolutions.com>
2020-01-15 12:02:51 -0800

    add simple game/assets

---
+++ README.md
@@ -0,0 +1,4 @@
+## Assets
+
+- https://pixabay.com/vectors/animal-anthropomorphic-cartoon-2029280/
+- https://freesound.org/people/qubodup/sounds/442907/
---
+++ index.js
@@ -0,0 +1,64 @@
+var config = {
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 640 },
+      debug: false
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+var player;
+var gruntSound;
+var platforms;
+var cursors;
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('player', './assets/pig.png');
+  this.load.audio('grunt', './assets/pigGrunt.wav');
+}
+
+function create() {
+  gruntSound = this.sound.add('grunt');
+
+  // The player and its settings
+  player = this.physics.add.sprite(100, 450, 'player');
+  player.displayHeight = 64;
+  player.displayWidth = 64;
+
+  //  Player physics properties. Give the little guy a slight bounce.
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  //  Input Events
+  cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.flipX = true;
+  }
+  else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.flipX = false;
+  }
+  else {
+    player.setVelocityX(0);
+  }
+
+  if (cursors.up.isDown && player.body.blocked.down) {
+    player.setVelocityY(-480);
+    gruntSound.play();
+  }
+}
